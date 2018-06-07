64d4ea8d638078861e034565f5e34b58a9b5956f
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-06-07 11:39:53 +0200

    add a view for showing dissertations

--- dissertation/urls.py
+++ dissertation/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # ex: /diss/
+    path('diss/', views.dissertation_list, name='dissertation_list'),
+    # ex: /diss/5/
+    path('diss/<int:diss_id>/', views.detail, name='detail'),
+    # ex: /diss/5/results/
+    path('diss/<int:diss_id>/results/', views.results, name='results'),
+    # ex: /diss/5/vote/
+    path('diss/<int:diss_id>/vote/', views.vote, name='vote'),
 ]
--- dissertation/views.py
+++ dissertation/views.py
@@ -1,5 +1,34 @@
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Dissertation
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the dissertation index.")
+def dissertation_list(request):
+    dlist = Dissertation.objects.order_by('title')
+    template = loader.get_template('index.html')
+    context = {
+        'dlist': dlist,
+    }
+    return HttpResponse(template.render(context, request))
+
+# def dissertation_list(request):
+#     dlist = Dissertation.objects.order_by('title')
+#     output = ', '.join([d.title for d in dlist])
+#     return HttpResponse(output)
+    
+def detail(request, diss_id):
+    diss = Dissertation.objects.filter(id=diss_id)
+    # import pdb; pdb.set_trace()
+    dtitle = [d.__str__() for d in diss]
+    # dtitle = [d.title for d in diss][0]
+    output = "You're looking at dissertation {}. The title is {}".format(
+        diss_id, dtitle)
+    return HttpResponse(output)
+
+def results(request, diss_id):
+    response = "You're looking at the results of dissertation {}.".format(diss_id)
+    return HttpResponse(response)
+    
+
+def vote(request, diss_id):
+    return HttpResponse("You're voting on dissertation {}.".format(diss_id))
