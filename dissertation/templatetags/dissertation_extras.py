268f324de126234d625adffa8b3e0bba270e7099
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-06-12 22:56:14 +0200

    add housestyle includes

---
+++ dissertation/templatetags/dissertation_extras.py
@@ -0,0 +1,12 @@
+import requests
+
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.simple_tag
+def include_external(url):
+	include = requests.get(url)
+	include_safe = mark_safe(include.text)
+	return include_safe
